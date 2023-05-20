1f5bc07a24070b8824d48b22b878509f696256a9
Krishna311203 <krishnashrivastava84@gmail.com>
2023-05-20 22:28:07 +0530

    added pages and project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hostel_permission/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: HomePage(),
     );
   }
 }
---
+++ lib/pages/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.amber,
+          title: const Text('Hostel Permission'),
+          centerTitle: true,
+          elevation: 0,
+          leading: Icon(Icons.menu_rounded)),
+    );
+  }
+}
